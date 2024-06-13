6e565b9f50c0ae9479a8351c4be8f9831e921a38
Pavel Kuzmin <Virus191288@gmail.com>
2024-06-13 11:35:46 +0300

    test formula

--- homebrew/Formula/rocksdb_fusion.rb
+++ homebrew/Formula/rocksdb_fusion.rb
@@ -1,24 +1,24 @@
 class RocksdbFusion < Formula
   desc "RocksDB server is designed to handle database operations over TCP connections"
   homepage "https://github.com/s00d/RocksDBFusion"
-  version "#{version}"  # Эта версия будет заменена через GitHub Actions
+  version "${version}"  # Эта версия будет заменена через GitHub Actions
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/s00d/RocksDBFusion/releases/download/server-v#{version}/rocksdb_server-Darwin-x86_64.tar.gz"
+      url "https://github.com/s00d/RocksDBFusion/releases/download/server-v${version}/rocksdb_server-Darwin-x86_64.tar.gz"
       sha256 "${MACOS_X86_64_SHA256}"
     elsif Hardware::CPU.arm?
-      url "https://github.com/s00d/RocksDBFusion/releases/download/server-v#{version}/rocksdb_server-Darwin-aarch64.tar.gz"
+      url "https://github.com/s00d/RocksDBFusion/releases/download/server-v${version}/rocksdb_server-Darwin-aarch64.tar.gz"
       sha256 "${MACOS_AARCH64_SHA256}"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/s00d/RocksDBFusion/releases/download/server-v#{version}/rocksdb_server-Linux-x86_64-musl.tar.gz"
+      url "https://github.com/s00d/RocksDBFusion/releases/download/server-v${version}/rocksdb_server-Linux-x86_64-musl.tar.gz"
       sha256 "${LINUX_X86_64_SHA256}"
     elsif Hardware::CPU.arm?
-      url "https://github.com/s00d/RocksDBFusion/releases/download/server-v#{version}/rocksdb_server-Linux-aarch64-musl.tar.gz"
+      url "https://github.com/s00d/RocksDBFusion/releases/download/server-v${version}/rocksdb_server-Linux-aarch64-musl.tar.gz"
       sha256 "${LINUX_AARCH64_SHA256}"
     end
   end
