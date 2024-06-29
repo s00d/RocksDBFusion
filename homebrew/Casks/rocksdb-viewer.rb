19825e2b3b9610811a9c806c6801b235f9c07ce5
Pavel Kuzmin <Virus191288@gmail.com>
2024-06-29 19:15:48 +0500

    fix

--- homebrew/Casks/rocksdb-viewer.rb
+++ homebrew/Casks/rocksdb-viewer.rb
@@ -3,8 +3,8 @@ cask "rocksdb-viewer" do
   sha256 "{MACOS_X86_64_SHA256}", macos: :monterey
   sha256 "{MACOS_AARCH64_SHA256}", macos: :arm64
 
-  url "https://github.com/s00d/RocksDBFusion/releases/download/app-v#{version}/rocksdb-viewer_#{version}_x64.dmg", if Hardware::CPU.intel?
-  url "https://github.com/s00d/RocksDBFusion/releases/download/app-v#{version}/rocksdb-viewer_#{version}_aarch64.dmg", if Hardware::CPU.arm?
+  url "https://github.com/s00d/RocksDBFusion/releases/download/app-v{VERSION}/rocksdb-viewer_{VERSION}_x64.dmg", if Hardware::CPU.intel?
+  url "https://github.com/s00d/RocksDBFusion/releases/download/app-v{VERSION}/rocksdb-viewer_{VERSION}_aarch64.dmg", if Hardware::CPU.arm?
 
   name "RocksDB Viewer"
   desc "A simple Tauri application to view and interact with a RocksDB database"
