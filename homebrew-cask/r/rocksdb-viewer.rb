0ff6590ee6f47e05871f1a33a39528925a83002e
Pavel Kuzmin <Virus191288@gmail.com>
2024-07-02 10:11:35 +0500

    fix

---
+++ homebrew-cask/r/rocksdb-viewer.rb
@@ -0,0 +1,40 @@
+cask "rocksdb-viewer" do
+  version "{VERSION}"
+
+  on_intel do
+    sha256 "{MACOS_X86_64_SHA256}"
+    url "https://github.com/s00d/RocksDBFusion/releases/download/app-v{VERSION}/rocksdb-viewer_{VERSION}_x64.dmg"
+  end
+
+  on_arm do
+    sha256 "{MACOS_AARCH64_SHA256}"
+    url "https://github.com/s00d/RocksDBFusion/releases/download/app-v{VERSION}/rocksdb-viewer_{VERSION}_aarch64.dmg"
+  end
+
+  name "RocksDB Viewer"
+  desc "A simple Tauri application to view and interact with a RocksDB database"
+  homepage "https://github.com/s00d/RocksDBFusion"
+
+  app "rocksdb-viewer.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{staged_path}/rocksdb-viewer.app"],
+                   sudo: true
+
+    system_command "/usr/bin/codesign",
+                   args: ["--force", "--deep", "--sign", "-", "#{staged_path}/rocksdb-viewer.app"],
+                   sudo: true
+  end
+
+  zap trash: [
+    "~/Library/Preferences/com.rocksdb.viewer.plist",
+    "~/Library/Saved Application State/com.rocksdb.viewer.savedState",
+  ]
+
+  caveats <<~EOS
+    During the installation process, you will be prompted to enter your password.
+    This is necessary to clear extended attributes and to self-sign the application
+    using the `xattr` and `codesign` commands to ensure it runs correctly on macOS.
+  EOS
+end
